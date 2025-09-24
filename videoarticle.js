7e6711aa9a36e82afba44c147e0d6e5173ada6e7
HTB2 <148723129+HTB2@users.noreply.github.com>
2025-09-25 00:34:09 +0100

    Update videoarticle.js

--- videoarticle.js
+++ videoarticle.js
@@ -4,4 +4,18 @@ for (var i = 0; i<= thumbTitle.length; i++){
         var shortendTitle =thumbTitle[i].innerHTML.slice(0, 50);
         thumbTitle[i].innerHTML = shortendTitle.concat("...");
     }
+
 }
+
+$(document).ready(function(){
+  $(".thumb-wrap").slice(0,32).show();
+  $("#load-more").click(function(e){
+    e.preventDefault();
+    $(".thumb-wrap:hidden").slice(0,32).fadeIn("slow");
+    // or make ajax call and append 
+    
+    if($(".thumb-wrap:hidden").length == 0){
+       $("#load-more").fadeOut("slow");
+      }
+  });
+})
